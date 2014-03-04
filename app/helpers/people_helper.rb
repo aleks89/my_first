731fbcea5ffe06109ea3c3c85affbe8fcff85325
aleks89 <sanbox77@mail.ru>
2014-03-04 19:12:58 +0400

    update

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,14 +1,13 @@
 module PeopleHelper
-
   def project_title_links(project)
   content_tag :h1 do
     [ project.title,
-    link_to_icon('show',project),
-    link_to_icon('edit',edit_project_path(project)),
-    link_to_icon('destroy',project, {
+    link_to_icon('show', project),
+    link_to_icon('edit', edit_project_path(project)),
+    link_to_icon('destroy', project, {
       :confirm=>'Are you sure?',
       :method=>:delete })
-      ].join(' ')
+      ].join(' ').html_safe
     end
   end
 end
